10fd99743f0a1b56a1a286c53bdcf36f246bcadd
osamakhalil98 <mozzbozz35@gmail.com>
2020-03-29 20:42:52 +0200

    hi

--- database/db.js
+++ database/db.js
@@ -3,6 +3,7 @@ const winston = require("winston");
 module.exports = function() {
   mongoose.set("useFindAndModify", false);
   mongoose.set("useCreateIndex", true);
+  mongoose.set("useUnifiedTopology", true);
   mongoose
     .connect("mongodb://localhost/auth", { useNewUrlParser: true })
     .then(winston.info("connected to mongodb.."));
