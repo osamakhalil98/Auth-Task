9d7ba66523148aad963f25424932b2988b107680
osamakhalil98 <mozzbozz35@gmail.com>
2020-03-29 22:02:12 +0200

    kk

--- database/db.js
+++ database/db.js
@@ -10,7 +10,7 @@ module.exports = async function() {
     "mongodb+srv://theosadxen:nahar2011@cluster0-a5rdm.mongodb.net/test?retryWrites=true&w=majority";
   await mongoose
     .connect(uri, {
-      useUnifiedTopology: false,
+      useUnifiedTopology: true,
       useCreateIndex: true,
       useNewUrlParser: true
     })
