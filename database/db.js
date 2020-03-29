afb12abe7b8864271752954f3602a9b023595298
osamakhalil98 <mozzbozz35@gmail.com>
2020-03-29 20:38:11 +0200

    working

--- database/db.js
+++ database/db.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 const winston = require("winston");
 module.exports = function() {
-  mongoose.set("useNewUrlParser", true);
   mongoose.set("useFindAndModify", false);
   mongoose.set("useCreateIndex", true);
-  mongoose.set("useUnifiedTopology", true);
   mongoose
     .connect("mongodb://localhost/auth", { useNewUrlParser: true })
     .then(winston.info("connected to mongodb.."));
