40c056d41ef7a2e7291b94943e5e5515fa7d683e
osamakhalil98 <mozzbozz35@gmail.com>
2020-03-29 21:49:21 +0200

    yaraaaab

--- database/db.js
+++ database/db.js
@@ -3,11 +3,11 @@ const winston = require("winston");
 const config = require("config");
 module.exports = async function() {
   mongoose.set("useFindAndModify", false);
-  mongoose.set("useNewUrlParser", true);
-  mongoose.set("useCreateIndex", true);
-  mongoose.set("useUnifiedTopology", true);
+  //mongoose.set("useUnifiedTopology", true);
   //const db = config.get("db");
   const uri =
     "mongodb+srv://theosadxen:nahar2011@cluster0-a5rdm.mongodb.net/test?retryWrites=true&w=majority";
-  await mongoose.connect(uri).then(() => winston.info(`Connected to ${db}...`));
+  await mongoose
+    .createConnection(uri, { useCreateIndex: true, useNewUrlParser: true })
+    .then(() => winston.info(`Connected to ${db}...`));
 };
