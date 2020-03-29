7037057d1d19a47c5dfea82a32fb252c06e958d1
osamakhalil98 <mozzbozz35@gmail.com>
2020-03-29 20:31:41 +0200

    works

--- startups/config.js
+++ startups/config.js
@@ -1,7 +1,7 @@
 const config = require("config");
 const winston = require("winston");
 module.exports = function() {
-  if (!config.get("jwt-privateKey")) {
+  if (!config.get("jwtprivateKey")) {
     throw new Error("ERROR:JWT PRIVATE KEY IS NOT DEFINED");
   }
 };
