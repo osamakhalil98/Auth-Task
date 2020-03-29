28d932bad13b246210904c0277a557d79b1d3c42
osamakhalil98 <mozzbozz35@gmail.com>
2020-03-29 21:01:45 +0200

    hope it works please

--- database/db.js
+++ database/db.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 const winston = require("winston");
+const config = require("config");
 module.exports = function() {
   mongoose.set("useFindAndModify", false);
+  mongoose.set("useNewUrlParser", true);
   mongoose.set("useCreateIndex", true);
   mongoose.set("useUnifiedTopology", true);
-  mongoose
-    .connect("mongodb://localhost/auth", { useNewUrlParser: true })
-    .then(winston.info("connected to mongodb.."));
+  const db = config.get("db");
+  mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
 };
