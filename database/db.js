39729f0fdea91827c03be8e39b5c64495a8e28e2
osamakhalil98 <mozzbozz35@gmail.com>
2020-03-29 21:42:14 +0200

    :(

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const winston = require("winston");
 const config = require("config");
-module.exports = function() {
+module.exports = async function() {
   mongoose.set("useFindAndModify", false);
   mongoose.set("useNewUrlParser", true);
   mongoose.set("useCreateIndex", true);
@@ -9,5 +9,5 @@ module.exports = function() {
   //const db = config.get("db");
   const uri =
     "mongodb+srv://theosadxen:nahar2011@cluster0-a5rdm.mongodb.net/test?retryWrites=true&w=majority";
-  mongoose.connect(uri).then(() => winston.info(`Connected to ${db}...`));
+  await mongoose.connect(uri).then(() => winston.info(`Connected to ${db}...`));
 };
